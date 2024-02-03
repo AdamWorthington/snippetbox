0362725410873a71ac71ea02438784c4a089abaa
adamworthington <aaworth111@gmail.com>
2024-02-02 22:48:49 -0800

    Added static files.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,15 +13,20 @@ func home(w http.ResponseWriter, r *http.Request) {
         http.NotFound(w, r)
         return
     }
+    files := []string{
+        "./ui/html/base.html",
+        "./ui/html/partials/nav.html",
+        "./ui/html/pages/home.html",
+    }
 
-    ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+    ts, err := template.ParseFiles(files...)
     if err != nil {
         log.Print(err.Error())
         http.Error(w, "Internal server error", http.StatusInternalServerError)
         return
     }
     
-    err = ts.Execute(w, nil)
+    err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
         log.Print(err.Error())
         http.Error(w, "Internal server error", http.StatusInternalServerError)
