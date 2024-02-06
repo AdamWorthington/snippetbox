b86c3f64a850c3b8c44285ebb6e7ebafe74aadb6
adamworthington <aaworth111@gmail.com>
2024-02-05 21:38:41 -0800

    Seperated out routing to routes.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,17 +20,10 @@ func main() {
     app := &application{
         logger: logger,
     }
-    mux := http.NewServeMux()
-    fileserver := http.FileServer(http.Dir("./ui/static/"))
-    mux.Handle("/static/", http.StripPrefix("/static", fileserver))
-
-    mux.HandleFunc("/", app.home)
-    mux.HandleFunc("/snipper/view", app.snippetView)
-    mux.HandleFunc("/snippet/create", app.snippetCreate)
 
     logger.Info("Starting server on port", "addr", *addr)
 
-    err := http.ListenAndServe(*addr, mux)
+    err := http.ListenAndServe(*addr, app.routes())
     logger.Error(err.Error())
     os.Exit(1)
 }   
