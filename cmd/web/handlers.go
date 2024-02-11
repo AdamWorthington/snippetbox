8422c574cde5fe20feb4a8ad48deb289d6387577
adamworthington <aaworth111@gmail.com>
2024-02-11 11:05:34 -0800

    Linked view endpoint to query up to the 10 latest records

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+    "errors"
     "fmt"
-    "html/template"
     "net/http"
     "strconv"
+    
+    "snippetbox.adamworthington.net/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -12,31 +14,33 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
         app.notFound(w)
         return
     }
-    files := []string{
-        "./ui/html/base.html",
-        "./ui/html/partials/nav.html",
-        "./ui/html/pages/home.html",
-    }
+    snippets, err := app.snippets.Latest()
 
-    ts, err := template.ParseFiles(files...)
     if err != nil {
         app.serveError(w, r, err)
         return
     }
-    
-    err = ts.ExecuteTemplate(w, "base", nil)
-    if err != nil {
-        app.serveError(w, r, err)
+    for _, snippet := range snippets {
+        fmt.Fprintf(w, "%+v\n", snippet)
     }
-
 }
+
 func (app *application) snippetView(w http.ResponseWriter, r * http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
         app.notFound(w)
         return
     }
-    fmt.Fprintf(w, "Display a snippet with id %d...", id)
+    snippet, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            app.notFound(w)
+        } else {
+            app.serveError(w, r, err)
+        }
+        return
+    }
+    fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) { 
--- internal/models/snippet.go
+++ internal/models/snippet.go
@@ -1,8 +1,9 @@
 package models
 
 import (
-    "database/sql"
-    "time"
+	"database/sql"
+	"errors"
+	"time"
 )
 
 type Snippet struct {
@@ -16,6 +17,7 @@ type Snippet struct {
 type SnippetModel struct {
     DB *sql.DB
 }
+
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
     stmt := `INSERT INTO snippets (title, content, created, expires) VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
     result, err := m.DB.Exec(stmt, title, content, expires)
@@ -31,9 +33,42 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-    return Snippet{}, nil
+    stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() AND id = ?`
+    row := m.DB.QueryRow(stmt, id)
+    var s Snippet
+    err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+    if err != nil {
+        if errors.Is(err, sql.ErrNoRows) {
+            return Snippet{}, ErrNoRecord
+        } else {
+            return Snippet{}, err
+        }
+    }
+    
+    return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-    return nil, nil
+    stmt := `Select id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+    rows, err := m.DB.Query(stmt)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    var snippets []Snippet
+    for rows.Next() {
+        var s Snippet
+
+        err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+        if err != nil {
+            return nil, err
+        }
+        snippets = append(snippets, s)
+    }
+    err = rows.Err()
+    if err != nil {
+        return nil, err
+    }
+    return snippets, nil
 }
