9767974e13698094115673d8ce80f1affd68876f
adamworthington <aaworth111@gmail.com>
2024-02-13 22:35:48 -0800

    Integrated httprouter

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,13 +7,10 @@ import (
     "strconv"
     
     "snippetbox.adamworthington.net/internal/models"
+    "github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        app.notFound(w)
-        return
-    }
     snippets, err := app.snippets.Latest()
     if err != nil {
         app.serveError(w, r, err)
@@ -27,7 +24,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r * http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    params := httprouter.ParamsFromContext(r.Context())
+    id, err := strconv.Atoi(params.ByName("id"))
     if err != nil || id < 1 {
         app.notFound(w)
         return
@@ -48,11 +46,9 @@ func (app *application) snippetView(w http.ResponseWriter, r * http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) { 
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        app.clientError(w, http.StatusMethodNotAllowed)
-        return
-    }
+    w.Write([]byte("display something better"))
+}
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) { 
     title := "0 snail"
     content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa" 
     expires := 7
@@ -62,6 +58,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
         app.serveError(w, r, err)
         return
     }
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+    http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }   
 
