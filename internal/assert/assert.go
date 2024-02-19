fd8843e16d29178ffe6ae3733dd931e874b86d14
adamworthington <aaworth111@gmail.com>
2024-02-19 11:16:35 -0800

    Added integration tests and test db scripts

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -2,7 +2,24 @@ package assert
 
 import (
     "testing"
+    "strings"
 )
+
+func NilError(t *testing.T, actual error) {
+    t.Helper()
+
+    if actual != nil {
+        t.Errorf("got %v, expected nil", actual)
+    }
+}
+
+func StringContains(t *testing.T, str, substr string) {
+    t.Helper()
+
+    if !strings.Contains(str, substr) {
+        t.Errorf("%v not found in: %v", substr, str)
+    }
+}
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
     t.Helper()
